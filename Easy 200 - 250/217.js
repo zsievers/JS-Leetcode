bd5c738ff1361bc7a56aa39eb51141df8a0291b1
Zach Sievers <zach.sievers@serrala.com>
2022-05-04 17:21:07 -0400

    217

---
+++ Easy 200 - 250/217.js
@@ -0,0 +1,18 @@
+function containsDup(nums) {
+  const set = new Set();
+  for (let n of nums) {
+    if (set.has(n)) return true;
+    set.add(n);
+  }
+  return false;
+}
+
+console.log(containsDup([1, 2, 3, 1])); // true
+console.log(containsDup([1, 2, 3, 4])); // false
+console.log(containsDup([1, 1, 2, 2, 3, 3, 44, 44])); // true
+
+/**
+ Other options
+ 1 - sort then compare current and next O(n) - but slow
+ 2 - hash map (not as quick as set)
+ */
